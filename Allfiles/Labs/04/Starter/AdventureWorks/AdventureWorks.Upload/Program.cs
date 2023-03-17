93d1c477477d65c1412e319b0929b19f8aea253f
Arturo Diaz Molina <jdiazmolina@microsoft.com>
2023-03-17 13:48:30 -0600

    Cmplees lab 4

--- Allfiles/Labs/04/Starter/AdventureWorks/AdventureWorks.Upload/Program.cs
+++ Allfiles/Labs/04/Starter/AdventureWorks/AdventureWorks.Upload/Program.cs
@@ -11,12 +11,12 @@
 
     public class Program
     {
-        private const string EndpointUrl = "";
-        private const string AuthorizationKey = "";
+        private const string EndpointUrl = "https://polycosmosarturo.documents.azure.com:443/";
+        private const string AuthorizationKey = "R0JnUb0ETnkghNkVQgLG0KPkzLBAQSA31jXMIWx12QZFl1vuRq9t4tN9R3EFa3GKLdUGqolcGraVACDblPTJqQ==";
         private const string DatabaseName = "Retail";
         private const string ContainerName = "Online";
-        private const string PartitionKey = "";
-        private const string JsonFilePath = "";
+        private const string PartitionKey = "/Category";
+        private const string JsonFilePath = "C:\\Repos\\AZ-204-DevelopingSolutionsforMicrosoftAzure\\Allfiles\\Labs\\04\\Starter\\AdventureWorks\\AdventureWorks.Upload\\models.json";
 
         static private int amountToInsert;
         static List<Model> models;
